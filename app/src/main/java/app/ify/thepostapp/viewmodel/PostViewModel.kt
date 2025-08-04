dcab6c09c1a93baa5dc7ba0974561f2978dd1d05
waynetipsy <jacketoniru@gmail.com>
2025-08-04 21:43:02 +0100

    Posts

--- app/src/main/java/app/ify/thepostapp/viewmodel/PostViewModel.kt
+++ app/src/main/java/app/ify/thepostapp/viewmodel/PostViewModel.kt
@@ -1,13 +1,34 @@
 package app.ify.thepostapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import app.ify.thepostapp.repo.PostRepository
+import app.ify.thepostapp.retrofit.Post
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+//Viewmodel updates when UI changes
 
 @HiltViewModel
 class PostViewModel @Inject constructor(
     private val postRepository: PostRepository
 ): ViewModel(){
+
+    private val _posts = MutableStateFlow<List<Post>>(emptyList())
+    val posts: StateFlow<List<Post>> get() = _posts
+
+    init {
+        fetchPosts()
+    }
+
+    private fun fetchPosts() {
+        viewModelScope.launch {
+                val posts = postRepository.getPosts()
+                _posts.value = posts
+
+        }
+    }
 }
