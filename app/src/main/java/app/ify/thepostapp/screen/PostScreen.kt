c35c76c5d0fbfc5e6e39dc105f9ce83785816ed0
waynetipsy <jacketoniru@gmail.com>
2025-08-06 21:20:51 +0100

    Comp

---
+++ app/src/main/java/app/ify/thepostapp/screen/PostScreen.kt
@@ -0,0 +1,34 @@
+package app.ify.thepostapp.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import app.ify.thepostapp.viewmodel.PostViewModel
+
+
+@Composable
+fun PostScreen(viewModel: PostViewModel = hiltViewModel()) {
+
+    // Observing Stateflow or Flow data from ViewModel
+    // it makes your compose UI react to flow changes
+    val posts = viewModel.posts.collectAsState().value
+
+    Column (modifier = Modifier.fillMaxSize()){
+        Text(text = "Posts App \n Hilt + Retrofit + MVVM",
+            modifier = Modifier.padding(top = 16.dp,
+                start = 24.dp), fontSize = 32.dp)
+
+        LazyColumn (modifier = Modifier.padding(16.dp)){
+          items(posts){ post ->
+              PostCard(post =post)
+          }
+        }
+    }
+}
